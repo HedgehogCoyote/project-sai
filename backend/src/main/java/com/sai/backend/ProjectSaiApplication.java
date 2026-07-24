0e7068e5bae1e58f2837498e350ab555cbd44670
강태호 <hedgehogcoyote@gmail.com>
2026-07-24 16:04:27 +0900

    chore : initalize project-sai folder

---
+++ backend/src/main/java/com/sai/backend/ProjectSaiApplication.java
@@ -0,0 +1,13 @@
+package com.sai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSaiApplication.class, args);
+	}
+
+}
