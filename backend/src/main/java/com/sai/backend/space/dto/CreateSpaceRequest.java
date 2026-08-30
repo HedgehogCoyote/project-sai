35ea20f0b30ce93431005d7731e2034b05802895
taeho <hedgehogcoyote@gmail.com>
2026-08-30 21:47:45 +0900

    fix : Change Space Title Length 100 -> 50

--- backend/src/main/java/com/sai/backend/space/dto/CreateSpaceRequest.java
+++ backend/src/main/java/com/sai/backend/space/dto/CreateSpaceRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record CreateSpaceRequest(
 		@NotBlank
-		@Size(min = 1, max = 30, message = "공간의 제목은 1자에서 30자 이내여야 합니다.")
+		@Size(min = 1, max = 50, message = "공간의 제목은 1자에서 30자 이내여야 합니다.")
 		String title	
 		) {
 }
